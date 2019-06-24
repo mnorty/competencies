5c16ef3f733edb91b3b844bce5efb30f62c918dc
mnorty <mnorty@gmail.com>
2019-06-24 10:16:22 -0600

    server up, connected to Database

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express'),
+      massive = require('massive')
+const app = express()
+const {CONNECTION_STRING,SERVER_PORT,SESSION_SECRET} = process.env
+
+app.use(express.json())
+
+massive(CONNECTION_STRING).then((db) => {
+  app.set('db',db)
+  console.log('All your Database are now belong to us',db.listTables())
+  app.listen(SERVER_PORT, () => console.log(`Listening on port:${SERVER_PORT}`))
+})
