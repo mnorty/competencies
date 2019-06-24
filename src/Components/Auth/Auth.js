b75a9d4904d6e459f81ad89b70a4bb2493fe01a7
mnorty <mnorty@gmail.com>
2019-06-24 11:59:27 -0600

    inputs update local state

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './Auth.css'
 
 
-const Auth = () => {
-    return (
+class Auth extends Component{
+    constructor(props){
+    super(props)
+    this.state={
+        username:'',
+        password:''
+    }
+    }
+
+    handleUsernameUpdate = (e) => {
+        console.log(this.state)
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    handlePasswordUpdate = (e) => {
+        console.log(this.state)
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    render(){
+        return(
     <div className='Auth'>
         <div className='authContainer'>
-
+           <input type="text" onChange={this.handleUsernameUpdate}></input>
+           <input type="text" onChange={this.handlePasswordUpdate}></input>
         </div>
     </div>
-    )}
+        )
+    }
+}
 
 export default Auth
